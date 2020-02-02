3cff7dc4bd850f84ae89cecc5c36c679116e6fc9
Alexanderqwer <llkkfi27456@gmail.com>
2020-02-02 22:26:43 +0200

    Updates

---
+++ precache-manifest.c6a747f5463fba19a6c5814f5a444c85.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c8aa662f5259ddb74b9ccbc0ac9918c",
+    "url": "/goit-react-hw-02-feedback/index.html"
+  },
+  {
+    "revision": "1ea54ddc1283dfcb118a",
+    "url": "/goit-react-hw-02-feedback/static/css/main.065022db.chunk.css"
+  },
+  {
+    "revision": "2d4c869eaf7e55bdc746",
+    "url": "/goit-react-hw-02-feedback/static/js/2.a65c27e8.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/goit-react-hw-02-feedback/static/js/2.a65c27e8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1ea54ddc1283dfcb118a",
+    "url": "/goit-react-hw-02-feedback/static/js/main.cbbd4cf0.chunk.js"
+  },
+  {
+    "revision": "c85e974bfb2f36ccba8d",
+    "url": "/goit-react-hw-02-feedback/static/js/runtime-main.221dd705.js"
+  }
+]);
